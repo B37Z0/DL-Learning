9313824aead60180a5846eca9bc51707c617d103
B37Z0 <benjz3721@gmail.com>
2026-06-20 21:24:18 -0400

    webcam check

--- CV/YOLO - Real Time OD/webcam.py
+++ CV/YOLO - Real Time OD/webcam.py
@@ -3,8 +3,12 @@ import cv2
 # Check for webcam 
 # 0: Integrated webcam
 # >1: External webcams
+cam_indices = []
+
 for i in range(5):
     cap = cv2.VideoCapture(i)
     if cap.read()[0]:
-        print(f"Camera found at index {i}")
+        cam_indices.append(i)
     cap.release()
+
+print("Webcams found at indices:", cam_indices)
